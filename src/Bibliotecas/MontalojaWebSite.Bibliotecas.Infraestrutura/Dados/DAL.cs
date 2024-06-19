e13473aa1f4159a7f033e15eb0adb86325766e48
Roberwal Junior <roberwal.junior@outlook.com>
2024-06-19 15:47:17 -0300

    Criando projeto de Infraestrutura com a classe de contexto e estrutura DAL para manipulação do banco de dados da aplicação.

---
+++ src/Bibliotecas/MontalojaWebSite.Bibliotecas.Infraestrutura/Dados/DAL.cs
@@ -0,0 +1,44 @@
+﻿namespace MontalojaWebSite.Bibliotecas.Infraestrutura.Dados;
+
+public class DAL<T> : IDisposable where T : class
+{
+    private readonly MontalojaWebSiteContext context;
+
+    public DAL(MontalojaWebSiteContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<T> Listar()
+    {
+        return context.Set<T>().ToList();
+    }
+
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
+    }
+
+    public void Adicionar(T objeto)
+    {
+        context.Set<T>().Add(objeto);
+        context.SaveChanges();
+    }
+
+    public void Atualizar(T objeto)
+    {
+        context.Set<T>().Update(objeto);
+        context.SaveChanges();
+    }
+
+    public void Deletar(T objeto)
+    {
+        context.Set<T>().Remove(objeto);
+        context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        context.Dispose();
+    }
+}
---
+++ src/Bibliotecas/MontalojaWebSite.Bibliotecas.Infraestrutura/Dados/MontalojaWebSiteContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MontalojaWebSite.Bibliotecas.Dominio.Modelos;
+
+namespace MontalojaWebSite.Bibliotecas.Infraestrutura.Dados;
+
+public class MontalojaWebSiteContext : DbContext
+{
+    public MontalojaWebSiteContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Loja> Lojas { get; set; }
+    public DbSet<Movel> Moveis { get; set; }
+}
